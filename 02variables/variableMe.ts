904d1a114a702271f330fd2346b33ecfbdbf87d0
Prabin <manandharprabin819@gmail.com>
2024-07-04 01:42:45 +0545

    till vid-6, any (always avoid)

--- 02variables/variableMe.ts
+++ 02variables/variableMe.ts
@@ -24,4 +24,21 @@ let isLoggedIn: boolean = false;
 let userId2: number = 334455; // too obvious, dont do this
 let userId3 = 334455; // typescript is smart enough to automatically detect that you want to put a number in it. and doesnt let you do => userId3 = "prabin"; in the next line
 userId3 = "33";
+
+//any               //always avoid any
+let hero1;
+
+function getHero1() {
+  return "thor";
+}
+hero1 = getHero1(); //here hero is of any type
+
+/* corollary*/
+//any               //always avoid any
+let hero2: string;
+
+function getHero2() {
+  return "thor";
+}
+hero2 = getHero2(); //here hero is of string type
 export {};
