28dfdefc81e5e51312de9d0109aed6b2de29f6ff
Prabin <manandharprabin819@gmail.com>
2024-07-07 11:40:27 +0545

    set up typescript for real projects

---
+++ 10setupTypescriptForRealProjects/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("typescript is here");
+//use "tsc -w" to generate the js file in the dist folder
+//otherwise index.js will be generated in the src folder
---
+++ 10setupTypescriptForRealProjects/src/index.ts
@@ -0,0 +1,4 @@
+console.log("typescript is here");
+//use "tsc -w" to generate the js file in the dist folder
+//otherwise index.js will be generated in the src folder
+//use ctrl+c to stop watch mode
